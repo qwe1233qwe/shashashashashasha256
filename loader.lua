8365ddd3e1cc8f17acfdf7956eec4cf4782fc890
qwe1233qwe <7@l11.store>
2026-08-25 23:02:16 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -13,7 +13,7 @@ if game.GameId == 73885730 then
 	loadstring(game:HttpGet("https://api.getsample.lol/files/prisonlife"))();
 elseif (game.GameId == 1008451066) then
 	loadstring(game:HttpGet("https://api.getsample.lol/files/dahood"))();
-elseif (game.GameId == 3634139746 then
+elseif (game.GameId == 3634139746) then
 	loadstring(game:HttpGet("https://cdn.getsample.lol/dvrwziox"))();
 elseif (game.GameId == 994732206) then
 	game.Players.LocalPlayer:Kick('SillyScripts Blox Fruits is down for now.'); 
